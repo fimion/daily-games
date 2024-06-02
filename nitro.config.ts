3dcf2319736185d5a925da2e1e0fefa368ed911e
Alex Riviere <fimion@gmail.com>
2024-06-02 02:02:26 -0400

    hook up storage correctly?

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,4 @@
 //https://nitro.unjs.io/config
-import * as Stuff from "@netlify/blobs";
-
-console.log(process.env);
 export default defineNitroConfig({
     srcDir: "server",
     serverAssets: [
@@ -14,7 +11,7 @@ export default defineNitroConfig({
     storage: {
         db: {
             driver: "netlifyBlobs",
-            name: "daily-games",
+            name: "db",
             deployScope: false,
         }
     },
@@ -30,7 +27,5 @@ export default defineNitroConfig({
         raindropApiToken: "RAINDROP_API_TOKEN",
         raindropGamesCollection: "RAINDROP_GAMES_COLLECTION",
         sessionPassword: "SESSION_PASSWORD_NEEDS_TO_BE_AT_LEAST_32_CHARACTERS_LONG",
-        siteId: process.env.SITE_ID,
-        netlifyApiToken: process.env.NETLIFY_API_TOKEN,
     }
 });
