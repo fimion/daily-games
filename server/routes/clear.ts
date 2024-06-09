6aa552b024607069e5186d9666f390f108187f49
Alex Riviere <fimion@gmail.com>
2024-06-09 08:30:24 -0400

    fix clear session

--- server/routes/clear.ts
+++ server/routes/clear.ts
@@ -2,6 +2,6 @@ import { useCurrentSession } from "~/utils/useCurrentSession.ts";
 
 export default defineEventHandler(async (event) => {
 	const session = await useCurrentSession(event);
-	await session.update({ lastID: undefined, lastAccessDay: undefined });
+	await session.clear();
 	return sendRedirect(event, "/", 303);
 });
