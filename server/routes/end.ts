1b54b70e892efedc844c3218f19d875dae551497
Alex Riviere <fimion@gmail.com>
2025-09-16 22:48:53 -0400

    add extras to end page

--- server/routes/end.ts
+++ server/routes/end.ts
@@ -1,16 +1,26 @@
 import { useCurrentSession } from "~/utils/useCurrentSession.ts";
 import { useLayout } from "~/utils/useLayout.ts";
-import { useShops } from "~/utils/storedLinks.ts";
+import { useShops, useGames, type StoredLinkItem } from "~/utils/storedLinks.ts";
+
+function itemsToList(items: StoredLinkItem[]) {
+	return `<ul>
+${items
+		.sort((a, b) => Math.sign(b.order - a.order))
+		.map((item) => `<li><a href="${item.link}" target="_blank">${item.name}</a></li>`)
+		.join("")}
+</ul>`
+}
 
 export default defineEventHandler(async (event) => {
 	await useCurrentSession(event);
 	const shops = await useShops();
-	const links =
-		"<ul>" +
-		shops
-			.sort((a, b) => Math.sign(b.order - a.order))
-			.map((item) => `<li><a href="${item.link}" target="_blank">${item.name}</a></li>`)
-			.join("") +
-		"</ul>";
+	const games = await useGames();
+	const extraGames = games.filter((item)=>item.tags.includes("extra"))
+
+	const links = {
+		content: itemsToList(shops),
+		extras:itemsToList(extraGames),
+	}
+
 	return await useLayout("end", links);
 });
--- server/utils/useLayout.ts
+++ server/utils/useLayout.ts
@@ -1,11 +1,21 @@
 import { useStorage } from "nitropack/runtime";
 
-export async function useLayout(partial: string, insertPartial: string = "", layout: string = "layout") {
+type InsertPartial = string | Record<string,string>;
+
+
+export async function useLayout(partial: string, insertPartial: InsertPartial = "", layout: string = "layout") {
 	const assets = useStorage("assets:server");
 	let layoutContent = await assets.getItem(`${layout}.html`);
 	let partialContent = await assets.getItem(`partials/${partial}.partial.html`);
 	if (typeof layoutContent === "string" && typeof partialContent === "string") {
-		partialContent = partialContent.replace(/\{\{content}}/gi, insertPartial);
+		if(typeof insertPartial === "string") {
+			partialContent = partialContent.replace(/\{\{content}}/gi, insertPartial);
+		} else {
+			for(const [key,content] of Object.entries(insertPartial)) {
+				partialContent = partialContent.replaceAll(`{{${key}}}`, content);
+			}
+		}
+
 		layoutContent = layoutContent.replace(/\{\{content}}/gi, partialContent);
 	}
 	return layoutContent;
